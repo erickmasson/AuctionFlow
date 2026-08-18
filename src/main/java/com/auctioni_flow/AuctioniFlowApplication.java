6e9152e28f504a5b4af07d9a8158e373bdc8a310
Erick Masson <erickmasson00@gmail.com>
2026-08-18 16:07:33 -0300

    feat: add docker-compose file

---
+++ src/main/java/com/auctioni_flow/AuctioniFlowApplication.java
@@ -0,0 +1,13 @@
+package com.auctioni_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctioniFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctioniFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auctioni_flow/AuctioniFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auctioni_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctioniFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
